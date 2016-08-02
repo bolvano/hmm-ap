ca7c2b4476af36958bf258275c34e66ba476d121
Nik <nik.panov.mobile@gmail.com>
2016-08-02 22:28:21 +0300

    added requirements + minor

--- tagger/views.py
+++ tagger/views.py
@@ -12,48 +12,47 @@ import ast
 
 def index(request):
     if request.method == "POST":
-        address = request.POST.get("address")
+        if request.POST.get("tokens"):
+            with open(settings.BASE_DIR+"/data/corpus.pkl", 'rb') as handle:
+                corpus = pickle.load(handle)
 
-        pkl_file = open(settings.BASE_DIR+"/data/hmm.pkl", 'rb')
-        hmm = pickle.load(pkl_file)
-        pkl_file.close()
+            tokens = ast.literal_eval(request.POST.get("tokens"))
+            tagged = []
+            i = 1
+            for item in tokens:
+                tagged.append((item,request.POST.get("token_"+str(i))))
+                i += 1
+            if tagged not in corpus:
+                corpus.append(tagged)
+                with open(settings.BASE_DIR+"/data/corpus.pkl", 'wb') as handle:
+                    pickle.dump(corpus, handle)
+                tag_set = unique_list(tag for sent in corpus for (word,tag) in sent)
+                symbols = unique_list(word for sent in corpus for (word,tag) in sent)
+                trainer = HiddenMarkovModelTrainer(tag_set, symbols)
+                hmm = trainer.train_supervised(corpus, estimator=LaplaceProbDist)
+                with open(settings.BASE_DIR+"/data/hmm.pkl", 'wb') as handle:
+                    pickle.dump(hmm, handle)
 
-        tokens = regexp_tokenize(address, pattern=r'\d+|[^\r\n\t\f 0-9]+', )
-        tagged = hmm.tag(tokens)
+            return render(request, 'tagger/index.html', {'corpus': corpus})
 
-        tags_file = open(settings.BASE_DIR+"/data/tags.json", 'rb')
+        else:
+            address = request.POST.get("address")
 
-        reader = codecs.getreader("utf-8")
-        tags = json.load(reader(tags_file))
+            pkl_file = open(settings.BASE_DIR+"/data/hmm.pkl", 'rb')
+            hmm = pickle.load(pkl_file)
+            pkl_file.close()
 
-        return render(request, 'tagger/index2.html', {'address': address,
-                                                      'tokens': tokens,
-                                                      'tagged': tagged,
-                                                      'tags': sorted(tags.items(), key=operator.itemgetter(1)) })
+            tokens = regexp_tokenize(address, pattern=r'\d+|[^\r\n\t\f 0-9]+', )
+            tagged = hmm.tag(tokens)
+
+            tags_file = open(settings.BASE_DIR+"/data/tags.json", 'rb')
+
+            reader = codecs.getreader("utf-8")
+            tags = json.load(reader(tags_file))
+
+            return render(request, 'tagger/index.html', {'address': address,
+                                                          'tokens': tokens,
+                                                          'tagged': tagged,
+                                                          'tags': sorted(tags.items(), key=operator.itemgetter(1)) })
 
     return render(request, 'tagger/index.html', {})
-
-def update(request):
-    if request.method == "POST":
-
-        with open(settings.BASE_DIR+"/data/corpus.pkl", 'rb') as handle:
-            corpus = pickle.load(handle)
-
-        tokens = ast.literal_eval(request.POST.get("tokens"))
-        tagged = []
-        i = 1
-        for item in tokens:
-            tagged.append((item,request.POST.get("token_"+str(i))))
-            i += 1
-        if tagged not in corpus:
-            corpus.append(tagged)
-            with open(settings.BASE_DIR+"/data/corpus.pkl", 'wb') as handle:
-                pickle.dump(corpus, handle)
-            tag_set = unique_list(tag for sent in corpus for (word,tag) in sent)
-            symbols = unique_list(word for sent in corpus for (word,tag) in sent)
-            trainer = HiddenMarkovModelTrainer(tag_set, symbols)
-            hmm = trainer.train_supervised(corpus, estimator=LaplaceProbDist)
-            with open(settings.BASE_DIR+"/data/hmm.pkl", 'wb') as handle:
-                pickle.dump(hmm, handle)
-
-        return render(request, 'tagger/index3.html', {'corpus': corpus})
