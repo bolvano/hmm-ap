a36b8d5372b7870f6d5e60468f1568a1ecc9da67
Jude Quinn <fromabbeyroad@yandex.ru>
2016-08-07 16:39:37 +0300

    CSS

--- addressparser/urls.py
+++ addressparser/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('tagger.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
