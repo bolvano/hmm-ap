ba8a9cd8698c2a5321fb774d6e7de59db7639317
Nik <nik.panov.mobile@gmail.com>
2016-08-09 20:14:13 +0300

    master added gitignore for .pkl
    master: added test vc
    
    master: removed pkls from repo

--- tagger/urls.py
+++ tagger/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^corpus$', views.corpus, name='corpus'),
+    url(r'^test$', views.test, name='test'),
 ]
--- tagger/views.py
+++ tagger/views.py
@@ -10,6 +10,7 @@ import codecs
 import operator
 import ast
 import random
+import uuid
 
 def get_random_address():
     with open(settings.BASE_DIR+"/data/raw.pkl", 'rb') as handle:
@@ -20,7 +21,25 @@ def get_random_address():
             pickle.dump(raw, handle)
         return a.decode()
     else:
-        return "АДРЕСА КОНЧИЛИСЬ("
+        return False
+
+def random_tag():
+    tags_file = open(settings.BASE_DIR+"/data/tags.json", 'rb')
+    reader = codecs.getreader("utf-8")
+    tags = json.load(reader(tags_file))
+    tags = sorted(tags.items(), key=operator.itemgetter(1))
+    return random.choice(tags)[0]
+
+def distort(tagged):
+    #import ipdb; ipdb.set_trace();
+    distorted = []
+    for t in tagged:
+        if int(random.getrandbits(1)) == 1:
+            rt = random_tag()
+            distorted.append((t[0],rt))
+        else:
+            distorted.append(t)
+    return distorted
 
 def corpus(request):
     with open(settings.BASE_DIR+"/data/corpus.pkl", 'rb') as handle:
@@ -62,6 +81,102 @@ def corpus(request):
     else:
         return render(request, 'tagger/corpus.html', {'corpus': corpus})
 
+def test(request):
+
+    if request.method == "POST":
+        session_id = request.session['session_id']
+        order = request.session['order']
+
+        from_corpus = request.session['from_corpus']
+        from_raw = request.session['from_raw']
+
+        tagged_valid = True
+        i = 1
+        for item in from_corpus:
+            if order == 1:
+                tag = request.POST.get("token_first_"+str(i))
+            else:
+                tag = request.POST.get("token_second_"+str(i))
+            if tag != from_corpus[i-1][1]:
+                tagged_valid = False
+            i += 1
+
+        if tagged_valid:
+
+            with open(settings.BASE_DIR+"/data/corpus.pkl", 'rb') as handle:
+                corpus = pickle.load(handle)
+
+            #import ipdb; ipdb.set_trace();
+
+            new_tagged = []
+            i = 1
+            for item in random_raw:
+                if order == 1:
+                    new_tagged.append((item[0],request.POST.get("token_second_"+str(i))))
+                else:
+                    new_tagged.append((item[0],request.POST.get("token_first_"+str(i))))
+                i += 1
+            if new_tagged not in corpus:
+                corpus.append(new_tagged)
+                with open(settings.BASE_DIR+"/data/corpus.pkl", 'wb') as handle:
+                    pickle.dump(corpus, handle)
+                tag_set = unique_list(tag for sent in corpus for (word,tag) in sent)
+                symbols = unique_list(word for sent in corpus for (word,tag) in sent)
+                trainer = HiddenMarkovModelTrainer(tag_set, symbols)
+                hmm = trainer.train_supervised(corpus, estimator=LaplaceProbDist)
+                with open(settings.BASE_DIR+"/data/hmm.pkl", 'wb') as handle:
+                    pickle.dump(hmm, handle)
+
+            return render(request, 'tagger/index.html', {'corpus': corpus})
+
+
+    request.session['order'] = order = int(random.getrandbits(1))
+
+    with open(settings.BASE_DIR+"/data/corpus.pkl", 'rb') as handle:
+        corpus = pickle.load(handle)
+        from_corpus = corpus.pop(random.randrange(len(corpus)))
+
+    from_corpus_str = ' '.join([t[0] for t in from_corpus])
+    from_corpus_str = from_corpus_str.replace(' , ',', ')
+
+    from_raw_str = get_random_address()
+    if not from_raw_str:
+        from_raw_str = corpus.pop(random.randrange(len(corpus)))
+
+    pkl_file = open(settings.BASE_DIR+"/data/hmm.pkl", 'rb')
+    hmm = pickle.load(pkl_file)
+    pkl_file.close()
+
+    tokens = regexp_tokenize(from_raw_str, pattern=r'\d+|[^\r\n\t\f 0-9,]+|,', )
+
+    from_raw = hmm.tag(tokens)
+
+    tags_file = open(settings.BASE_DIR+"/data/tags.json", 'rb')
+    reader = codecs.getreader("utf-8")
+    tags = json.load(reader(tags_file))
+    tags_file.close()
+
+    session_id = uuid.uuid1()
+    request.session['session_id'] = str(session_id)
+
+    request.session['from_corpus'] = from_corpus
+    request.session['from_raw'] = from_raw
+
+    from_corpus_distorted = distort(from_corpus)
+
+    data = [from_corpus_distorted, from_corpus_str, from_raw, from_raw_str]
+
+    return render(request, 'tagger/test.html', {
+
+        'data': data,
+
+        'session_id': session_id,
+        'order': order,
+
+        'tags': sorted(tags.items(), key=operator.itemgetter(1)) })
+
+
+
 def index(request):
     if request.method == "POST":
         if request.POST.get("tokens"):
@@ -90,6 +205,9 @@ def index(request):
         else:
             if request.POST.get("random") == 'true':
                 address = get_random_address()
+                if not address:
+                    return render(request, 'tagger/index.html', {'error_message': 'No random addresses left'})
+
             else:
                 address = request.POST.get("address")
 
@@ -100,13 +218,12 @@ def index(request):
                 hmm = pickle.load(pkl_file)
                 pkl_file.close()
 
-
                 tagged = hmm.tag(tokens)
 
                 tags_file = open(settings.BASE_DIR+"/data/tags.json", 'rb')
-
                 reader = codecs.getreader("utf-8")
                 tags = json.load(reader(tags_file))
+                tags_file.close()
 
                 return render(request, 'tagger/index.html', {'address': address,
                                                               'tokens': tokens,
@@ -114,5 +231,4 @@ def index(request):
                                                               'tags': sorted(tags.items(), key=operator.itemgetter(1)) })
 
     return render(request, 'tagger/index.html', {})
-
 
